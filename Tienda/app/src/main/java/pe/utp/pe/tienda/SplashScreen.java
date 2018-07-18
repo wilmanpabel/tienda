8599e16345644ac620e118e4c5ea92c1aed042dd
jackpinao <jack.pime@gmail.com>
2018-07-18 17:14:06 -0500

    5.1
    se agrego splash screen

---
+++ Tienda/app/src/main/java/pe/utp/pe/tienda/SplashScreen.java
@@ -0,0 +1,29 @@
+package pe.utp.pe.tienda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+
+}
